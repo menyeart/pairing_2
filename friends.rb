24c4a1364e0970b53f6196ac0c7ce247664b14ac
Matt Enyeart <menyeart1@gmail.com>
2022-11-02 14:00:41 -0600

    Add arrays

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ friends = ['bob', 'fred', 'janet', 'jomah', 'nick']
 puts friends[2]
 puts friends[-1]
 puts friends[0]
+
+places = ['japan', 'france', 'brazil', 'florida', 'cap cod']
+places.push('Texas')
+puts places
